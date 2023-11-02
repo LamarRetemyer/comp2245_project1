9e36ba4e45c29563f7369d59bbd5398009f29118
Lamar Retemyer <lamar.retemyer@gmail.com>
2023-11-02 00:34:39 -0400

    Finished up the code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    const messageDiv = document.querySelector('.message');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const emailInput = form.querySelector('input[name="email"]');
+        const userEmail = emailInput.value;
+
+        if (userEmail.trim() === '') {
+            messageDiv.textContent = 'Please enter a valid email address.';
+        } else {
+            messageDiv.textContent = `Thank you! Your email address ${userEmail} has been added to our mailing list!`;
+            emailInput.value = '';
+        }
+    });
+});
